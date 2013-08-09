55ed772fcba83cda2296f76a0f437b7ef2951c87
Matthew Hanson <matthew.hanson@zazzle.com>
2013-08-08 18:17:11 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = 'ea79ac0aed1c309716712907b76b9f48e44afb1e7df54526b474c77cce69ae9bda587f9752b01296dde9e75a55ff646576ab4a4e585bc4ba59b04970d4c08aca'
